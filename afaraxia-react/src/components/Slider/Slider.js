9feed140f0d8e7a0c32b0bbb5e425dda6e81e961
Cristobal Rodriguez <tobal.rodriguez@gmail.com>
2022-05-13 01:53:56 -0300

    slider terminado

--- afaraxia-react/src/components/Slider/Slider.js
+++ afaraxia-react/src/components/Slider/Slider.js
@@ -1,17 +1,40 @@
 import '../../styles/estilos.css';
+import SliderData from './sliderData';
+import { useState } from 'react'
 
 
 const Slider = () => {
+  const [slideIndex,setSlideIndex] = useState(0)
+
+  const handleWrap = (direction) => {
+    if(direction === "left"){
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+    }
+  }
+
+
+
   return (
     <div className="sliderContainer">
-        <div className="arrowContainer">
+        <div className="arrowContainer" onClick={()=>handleWrap('left')}>
             <i className="fa-solid fa-arrow-left"></i>
         </div>
-        <div className="wrapper">
-            <div className="imageContainer"><img src= '(https://www.pexels.com/es-es/foto/cinco-cactus-de-varios-colores-311458/)' alt="muchos cactus"/></div>
-            <div className="infoContainer"></div>
+        <div className={slideIndex === 1 ? "wrapper1" : slideIndex === 2 ? "wrapper2" : "wrapper"}>
+            {SliderData.map((data)=>(
+              <div className="slide">
+                <div className="imageContainer">
+                  <img src={data.img} alt={data.alt}/></div>
+                <div className="infoContainer">
+                  <h1 className="title"> {data.title}</h1>
+                  <p className= "description">{data.description}</p>
+                  <button className="buttonSlider">Descubrir</button>
+                </div>
+              </div>
+            ))}
         </div>
-        <div className="arrowContainer2">
+        <div className="arrowContainer2" onClick={()=>handleWrap('right')}>
             <i className="fa-solid fa-arrow-right"></i>
         </div>
     </div>
